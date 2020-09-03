3cdae0268d2538124ca90a774a1204e94ccb515c
Abdallah hamouda <68547931+AbdallahHamouda@users.noreply.github.com>
2020-09-03 16:56:46 +0300

    Add files via upload

---
+++ precache-manifest.94de9314e01c7fc4a3328502cd62495d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca1ebff50de5b193c5a",
+    "url": "/css/about.21c0ed9d.css"
+  },
+  {
+    "revision": "72cd64f3c2b1b05a7af2",
+    "url": "/css/app.7b14fe88.css"
+  },
+  {
+    "revision": "b450d2dda9a7dbd972cc697a5dcd90c1",
+    "url": "/fav.png"
+  },
+  {
+    "revision": "5ec796ade46a4e36b7f190398895fa92",
+    "url": "/img/twitter.5ec796ad.png"
+  },
+  {
+    "revision": "3c8488a36fbb561499140c04e6fb33f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ca1ebff50de5b193c5a",
+    "url": "/js/about.4a203fcd.js"
+  },
+  {
+    "revision": "72cd64f3c2b1b05a7af2",
+    "url": "/js/app.7f11284e.js"
+  },
+  {
+    "revision": "ba5bbbb48c8b84687631",
+    "url": "/js/chunk-vendors.dafd9d2f.js"
+  },
+  {
+    "revision": "11b03a5928532cb54b37460590580ccb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
